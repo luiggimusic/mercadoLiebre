c38df968555b1ae730b9386633b82c0e08a8a711
luiggimusic <--global>
2022-07-05 02:51:44 +0200

    Se borra gitignore

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 const path = require('path');
 const publicPath = path.join(__dirname, 'public');
 app.use(express.static(publicPath));
 
-app.listen(3000, console.log("El servidor está vivo en el puerto 3000"));
+app.listen(port, () => console.log(`Server running in port ${port}`));
 
 
 
